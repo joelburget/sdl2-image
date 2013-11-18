c8a373b14ad38e1c5d05192660ceb694022228d2
Joel Burget <joel@khanacademy.org>
2013-11-17 17:50:39 -0800

    Initial commit.
    Updated to work with SDL2.

---
+++ config.mk
@@ -0,0 +1,5 @@
+SDL_CFLAGS=-I/usr/local/include/SDL2 -D_THREAD_SAFE
+SDL_LIBS=/usr/local/lib/libSDL_image.dylib
+PACKAGE=SDL2-image
+VERSION=0.1.0
+MAINTAINER=joelburget@gmail.com
---
+++ includes/HsSDLConfig.h
@@ -0,0 +1,23 @@
+/* includes/HsSDLConfig.h.  Generated from HsSDLConfig.h.in by configure.  */
+/* includes/HsSDLConfig.h.in.  Generated from configure.ac by autoheader.  */
+
+/* Define to 1 if you have the `SDL_image' library (-lSDL_image). */
+#define HAVE_LIBSDL_IMAGE 1
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "joelburget@gmail.com"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "SDL2-image"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "SDL2-image 0.1.0"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "SDL2-image"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL ""
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "0.1.0"
